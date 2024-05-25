6d49ac0963a2b5fb2fb87dd1702e162b2cb0ea95
Sayaf Hameed Sheikh <sheikhsayaf@gmail.com>
2024-05-25 12:32:30 +0530

    scroll reveal

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container , .project-box , .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-img, .services-container , .project-box , .contact form , .skill-box', { origin: 'bottom'});
 ScrollReveal().reveal('.btn', { origin: 'bottom'});
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
